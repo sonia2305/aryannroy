94f22318870942847e20241f2624ccb0b85053ad
Sonia Raychaudhuri <sonia2305@gmail.com>
2016-02-28 22:10:31 +0530

    adding subsections

--- Portfolio/app/app.module.js
+++ Portfolio/app/app.module.js
@@ -1,13 +1,62 @@
-var app = angular.module("mainModule", [ 'ngRoute', 'controllers' ]);
+var app = angular.module("mainModule", [ 'ui.router', 'ngSanitize', 'ui.bootstrap' ]);
 
-app.config([ '$routeProvider', function($routeProvider) {
-	$routeProvider.when('/sections', {
-		templateUrl : 'app/components/home/SectionsPage.html',
-		controller : 'MainController'
-	}).
+app.config(function($stateProvider, $urlRouterProvider) {
 
-	otherwise({
-		redirectTo : '/sections'
+	  // Redirection for unmatched url
+	  $urlRouterProvider.otherwise("/sections");
+
+	  // Setting up States
+	  $stateProvider
+	    .state('Home', {
+	      url: "/sections",
+	      templateUrl: "app/components/home/SectionsPage.html",
+	      controller : "MainController"
+	    });
 	});
-} ]);
 
+app.directive('tab', function() {
+  return {
+	  restrict: 'E',
+	  transclude: true,
+	  template: '<div role="tabpanel" ng-show="active" ng-transclude></div>',
+	  require: '^tabset',
+	  scope: {
+	    heading: '@'
+	  },
+	  link: function(scope, elem, attr, tabsetCtrl) {
+	    scope.active = false
+	    tabsetCtrl.addTab(scope)
+	  }
+	}
+})
+		
+app.directive('tabset', function() {
+  return {
+	    restrict: 'E',
+	    transclude: true,
+	    scope: { },
+	    templateUrl: 'app/components/home/tabset.html',
+	    bindToController: true,
+	    controllerAs: 'tabset',
+	    controller: function() {
+	      var self = this
+	      self.tabs = []
+	      self.addTab = function addTab(tab) {
+	    	  self.tabs.push(tab)
+
+	    	  if(self.tabs.length === 1) {
+	    	    tab.active = true
+	    	  }
+	    	}
+	      self.select = function(selectedTab) {
+	    	  angular.forEach(self.tabs, function(tab) {
+	    	    if(tab.active && tab !== selectedTab) {
+	    	      tab.active = false;
+	    	    }
+	    	  })
+
+	    	  selectedTab.active = true;
+	    	}
+	    }
+	  }
+	})
--- Portfolio/app/components/home/MainController.js
+++ Portfolio/app/components/home/MainController.js
@@ -1,7 +1,5 @@
-var app = angular.module("controllers", ['ngSanitize']);
-
-app.controller("MainController", function($scope, $http, $sce) {
-			
+app.controller("MainController", function($scope, $http) {
+	
 	$http.get("app/shared/json/ApplicationProperties.json").success(function(response) {
 		$scope.title = response.title;
 		$scope.headerBackgroundImage = response.headerBackgroundImage;
@@ -14,11 +12,57 @@ app.controller("MainController", function($scope, $http, $sce) {
 	$http.get("app/shared/json/Sections.json").success(function(response) {
 		
 		angular.forEach(response, function(value, key) {
-			console.log("key:"+key+", value:"+JSON.stringify(value));
-			$scope.sections.push(value.SectionName);
+			$scope.sections.push(value);
 		});
 		
-		console.log(JSON.stringify($scope.sections));
 	});
 	
+	$scope.active="";
+	this.showTab = function(tabName){
+		console.log("clicked tab "+tabName);
+		$scope.active = tabName;
+	}
+	
+	
+	$scope.myInterval = 5000;
+	  $scope.noWrapSlides = false;
+	  $scope.active = 0;
+	  var currIndex = 0;
+
+	  $scope.randomize = function() {
+	    var indexes = generateIndexesArray();
+	    assignNewIndexesToSlides(indexes);
+	  };
+
+	  // Randomize logic below
+
+	  function assignNewIndexesToSlides(indexes) {
+	    for (var i = 0, l = slides.length; i < l; i++) {
+	      slides[i].id = indexes.pop();
+	    }
+	  }
+
+	  function generateIndexesArray() {
+	    var indexes = [];
+	    for (var i = 0; i < currIndex; ++i) {
+	      indexes[i] = i;
+	    }
+	    return shuffle(indexes);
+	  }
+
+	  // http://stackoverflow.com/questions/962802#962890
+	  function shuffle(array) {
+	    var tmp, current, top = array.length;
+
+	    if (top) {
+	      while (--top) {
+	        current = Math.floor(Math.random() * (top + 1));
+	        tmp = array[current];
+	        array[current] = array[top];
+	        array[top] = tmp;
+	      }
+	    }
+
+	    return array;
+	  }
 });
